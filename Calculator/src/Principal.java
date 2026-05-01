cbeb3b4aa4b98845c2df023312d6096e96818c67
Karen Silva Contreras <silvacontreraskaren@gmail.com>
2026-05-01 14:29:05 -0500

    cambio ojo 1

--- Calculator/src/Principal.java
+++ Calculator/src/Principal.java
@@ -17,7 +17,8 @@ public class Principal {
             System.out.println("1. Sumar");
             System.out.println("2. Restar");
             System.out.println("3. Salir");
-            System.out.println("Primer cambio ojo ");
+            System.out.println("Primer cambio ojo 1 ");
+            
             System.out.print("Elige una opción: ");
             opcion = Integer.parseInt(sc.nextLine());
 
